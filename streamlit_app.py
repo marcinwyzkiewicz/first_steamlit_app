06a203fb83385dc96f62b45b48a29de96d7d81df
marcinwyzkiewicz <44786935+marcinwyzkiewicz@users.noreply.github.com>
2023-02-10 03:17:04 +0100

    Update streamlit_app.py
    adding api data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
+import requests
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -15,10 +17,14 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-
 streamlit.header('Breakfast Menu 🥑🍞')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 
+streamlit.header("Fruityvice Fruit Advice!")
+
+streamlit.text(fruityvice_response.json())
+
+streamlit.text(fruityvice_response)
