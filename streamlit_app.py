328f1f2a7476d51a6bbc17c71e1afc3577357376
marcinwyzkiewicz <44786935+marcinwyzkiewicz@users.noreply.github.com>
2023-02-10 03:21:58 +0100

    Update streamlit_app.py
    comments added + changed request url

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,6 +25,9 @@ streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response) #status
 
-streamlit.text(fruityvice_response)
+# normalizing
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# table
+streamlit.dataframe(fruityvice_normalized)
